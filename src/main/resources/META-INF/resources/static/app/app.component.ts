4d245debc5fea25ef209b10945a3f26fc2048244
Heiko Scherrer <scherrer@openwms.org>
2016-03-25 22:18:07 +0100

    done with chapter about service integration

--- src/main/resources/META-INF/resources/static/app/app.component.ts
+++ src/main/resources/META-INF/resources/static/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
@@ -63,27 +64,24 @@ import {HeroDetailComponent} from './hero-detail.component';
     border-radius: 4px 0 0 4px;
   }
 `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
     public title = 'Tours of Heroes';
     selectedHero : Hero;
-    public heroes = HEROES;
+    heroes: Hero[];
+
+    constructor(private _heroService: HeroService) { }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
-
