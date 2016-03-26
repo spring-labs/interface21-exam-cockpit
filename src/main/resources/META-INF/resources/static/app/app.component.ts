addba0cdb1ea173b68a1ba83e267ee25dc829ac9
Heiko Scherrer <scherrer@openwms.org>
2016-03-26 11:32:45 +0100

    worked on routing

--- src/main/resources/META-INF/resources/static/app/app.component.ts
+++ src/main/resources/META-INF/resources/static/app/app.component.ts
@@ -1,16 +1,38 @@
 import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
-  `,
-  directives: [HeroesComponent],
-  providers: [
-    HeroService
-  ]
+        <h1>{{title}}</h1>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ]
 })
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }
+])
 export class AppComponent {
     
     title = 'Tour de Heroes';
