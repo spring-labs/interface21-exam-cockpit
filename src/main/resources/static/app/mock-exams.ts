0b34bc649d06fcd48527a3606643fa0367d50f8a
Heiko Scherrer <scherrer@openwms.org>
2016-04-08 23:08:53 +0200

    fixed bug; appid must be set in mock

--- src/main/resources/static/app/mock-exams.ts
+++ src/main/resources/static/app/mock-exams.ts
@@ -3,10 +3,11 @@ import { Question } from './question';
 import { Answer } from './answer';
 
 export var EXAMS: Exam[] = [
-    <Exam> { "id":  1, "examIdentifier": "SS-01/2015", "title": "Systeme & Signale (Q1)", "maxScorePoints": 20, "active": false, "version": "1.0",
+    <Exam> { "id":  1, "appid": 1, "examIdentifier": "SS-01/2015", "title": "Systeme & Signale (Q1)", "maxScorePoints": 20, "active": false, "version": "1.0",
     "questions": [
         <Question> {
             "id": 1,
+            "appid": 2,
             "text": "When did Napoleon die?",
             "order": 1,
             "scorePoints": 20,
@@ -14,6 +15,7 @@ export var EXAMS: Exam[] = [
             "answers": [ 
                 <Answer> {
                     "id": 1,
+                    "appid": 1,
                     "order": 1,
                     "text": "1782",
                     "dtype": "CHECKABLE",
@@ -21,6 +23,7 @@ export var EXAMS: Exam[] = [
                 },
                 <Answer> {
                     "id": 2,
+                    "appid": 2,
                     "order": 2,
                     "text": "1759",
                     "dtype": "CHECKABLE",
@@ -28,6 +31,7 @@ export var EXAMS: Exam[] = [
                 },
                 <Answer> {
                     "id": 3,
+                    "appid": 3,
                     "order": 3,
                     "text": "1821",
                     "dtype": "CHECKABLE",
@@ -35,6 +39,7 @@ export var EXAMS: Exam[] = [
                 },
                 <Answer> {
                     "id": 4,
+                    "appid": 4,
                     "order": 4,
                     "text": "1898",
                     "dtype": "CHECKABLE",
@@ -44,6 +49,7 @@ export var EXAMS: Exam[] = [
         },
         <Question> {
             "id": 2,
+            "appid": 2,
             "text": "When was Charlie Brown born?",
             "order": 2,
             "scorePoints": 10,
@@ -51,13 +57,13 @@ export var EXAMS: Exam[] = [
         }    
     
     ]},
-    <Exam> { "id":  2, "examIdentifier": "SS-05/2015", "title": "Systeme & Signale (Q2)", "maxScorePoints": 20, "active": true, "version": "1.1" },
-    <Exam> { "id":  3, "examIdentifier": "DF-01/2016", "title": "Digitale Filter", "maxScorePoints": 18, "active": false, "version": "1.2" },
-    <Exam> { "id":  4, "examIdentifier": "DF-04/2016", "title": "Digitale Filter", "maxScorePoints": 18, "active": true, "version": "2.0" },
-    <Exam> { "id":  5, "examIdentifier": "DS-04/2014", "title": "Digitale Signalverarbeitung", "maxScorePoints": 20, "active": false, "version": "1.0" },
-    <Exam> { "id":  6, "examIdentifier": "DS-01/2015", "title": "Digitale Signalverarbeitung", "maxScorePoints": 20, "active": false, "version": "1.1" },
-    <Exam> { "id":  7, "examIdentifier": "DS-04/2015", "title": "Digitale Signalverarbeitung", "maxScorePoints": 20, "active": true, "version": "2.0" },
-    <Exam> { "id":  8, "examIdentifier": "03/2015", "title": "Numerik I", "maxScorePoints": 20, "active": false, "version": "v1.0" },
-    <Exam> { "id":  9, "examIdentifier": "11/2015", "title": "Numerik I", "maxScorePoints": 20, "active": true, "version": "v2.0" },
-    <Exam> { "id": 10, "examIdentifier": "02/2015", "title": "Numerik II", "maxScorePoints": 18, "active": true, "version": "v1.0" }
+    <Exam> { "id":  2, "appid":  2, "examIdentifier": "SS-05/2015", "title": "Systeme & Signale (Q2)", "maxScorePoints": 20, "active": true, "version": "1.1" },
+    <Exam> { "id":  3, "appid":  3, "examIdentifier": "DF-01/2016", "title": "Digitale Filter", "maxScorePoints": 18, "active": false, "version": "1.2" },
+    <Exam> { "id":  4, "appid":  4, "examIdentifier": "DF-04/2016", "title": "Digitale Filter", "maxScorePoints": 18, "active": true, "version": "2.0" },
+    <Exam> { "id":  5, "appid":  5, "examIdentifier": "DS-04/2014", "title": "Digitale Signalverarbeitung", "maxScorePoints": 20, "active": false, "version": "1.0" },
+    <Exam> { "id":  6, "appid":  6, "examIdentifier": "DS-01/2015", "title": "Digitale Signalverarbeitung", "maxScorePoints": 20, "active": false, "version": "1.1" },
+    <Exam> { "id":  7, "appid":  7, "examIdentifier": "DS-04/2015", "title": "Digitale Signalverarbeitung", "maxScorePoints": 20, "active": true, "version": "2.0" },
+    <Exam> { "id":  8, "appid":  8, "examIdentifier": "03/2015", "title": "Numerik I", "maxScorePoints": 20, "active": false, "version": "v1.0" },
+    <Exam> { "id":  9, "appid":  9, "examIdentifier": "11/2015", "title": "Numerik I", "maxScorePoints": 20, "active": true, "version": "v2.0" },
+    <Exam> { "id": 10, "appid": 10, "examIdentifier": "02/2015", "title": "Numerik II", "maxScorePoints": 18, "active": true, "version": "v1.0" }
 ];
